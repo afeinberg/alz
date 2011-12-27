76c38760d87dde30175516069bbac6a33ddef2d9
Alex Feinberg <alex@strlen.net>
2011-12-27 14:37:22 -0800

    Improve documentation

--- src/encoder.cc
+++ src/encoder.cc
@@ -73,8 +73,59 @@ void Encoder::flush() {
 }
 
 
-// NOTE: *** This is slow ***
-
+/**
+ * Note: this is slow!
+ *
+ * My optimizations here were as follows:
+ *
+ * 1) Use a rolling hash type algorithm (see memmem_opt.c) vs. the
+ *    naive use of of memem() in libc. I also tried another algorithm,
+ *    Boyer-Moore-Horspool, but performed slightly worse than the
+ *    rolling hash algorithm used.
+ *
+ * 2) Using a hash table to keep track of previous matches. This took
+ *    several iterations. First approach was to naively use
+ *    std::unordered_map from STL, supplying a custom hash and equals
+ *    function. This actually ended up being _slower_ than not using a
+ *    hash table at all.
+ *
+ *    My next approach was to implement just the hash table buckets,
+ *    but no collision resolution algorithm (to avoid the overhead of
+ *    keeping a linked list for the chaining method of collision
+ *    resolution).
+ *
+ *    I then experimented with implementing chaining collision
+ *    resolution, using a memory pool for allocating the linked list
+ *    nodes. However, I found that given the small sliding window
+ *    (4096 bytes, i.e., 2^12) there was performance benefit (and even
+ *    a slight performance) cost to using chaining vs. simply ignoring
+ *    hash collisions (treating them as cache misses). This, of
+ *    course, came at the cost of higher memory utilization due to a
+ *    larger bucket table. If you note in the code, when ALZ_DEBUG_ is
+ *    enabled I keep track of hits and misses -- the percentage
+ *    increase in cache hits was only a few percent higher as result
+ *    of using chaining collision resolution.
+ *
+ *    Based on these results, I chose the "use just the hash table
+ *    buckets, no resolution, treat collisions as cache misses"
+ *    approach.
+ *
+ *    One approach I did not, however, experiment with was using a
+ *    very small fast to compute hash function (with lots of
+ *    collisions). The reason I didn't experiment with this is that
+ *    cost of computing the hash is fairly minor due to the cost of
+ *    finding the match by searching.
+ *
+ * After being told by Glen that I could look at other lz77
+ * implementations, I found that a better approach would have been to
+ * pre-build a data structure for efficient matches and then use that
+ * to do efficient searching for matches. This is similar to the
+ * Knuth-Morris-Pratt search algorithm, which is used by some lz77
+ * implementations.
+ *
+ * However, it looks like it would have required essentially a
+ * re-write of the structures I used to do the "sliding window".
+ */
 bool Encoder::find_match(const char *inp, size_t look_ahead) {
    
     if (find_in_hash(inp, look_ahead)) {
@@ -119,6 +170,7 @@ bool Encoder::find_match(const char *inp, size_t look_ahead) {
     return false;
 }
 
+// Ugly code, essentially wraps the search function
 bool Encoder::find_in_window(const char *haystack,
                              size_t haystack_len,
                              const char *needle,
